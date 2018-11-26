b82ab2cc73f2eb859a1c4e9a6307ce5097744383
HsiangHo <ho_cheung@qq.com>
2018-11-26 21:29:03 +0800

    added demo

--- SpotlightActionKit/SpotlightActionDemo/AppDelegate.m
+++ SpotlightActionKit/SpotlightActionDemo/AppDelegate.m
@@ -7,16 +7,26 @@
 //
 
 #import "AppDelegate.h"
+#import <SpotlightActionKit/SpotlightActionKit.h>
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate{
+    SASpotlightActionMaker          *_actionMaker;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    NSString *loaderPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingString:@"/SpotlightActionLoader.app"];
+    SASpotligjtActionConfigure *configure = [[SASpotligjtActionConfigure alloc] init];
+    [configure setName:@"test"];
+    [configure setIcon:[NSImage imageNamed:@"cat"]];
+    [configure setPath:[[[NSBundle mainBundle] resourcePath] stringByAppendingString:@"/scpt"]];
+    _actionMaker = [[SASpotlightActionMaker alloc] initWithActionLoaderPath:loaderPath];
+    [_actionMaker makeActionWithConfigure:configure outPutPath:@"/Users/jovi/Desktop/"];
 }
 
 
